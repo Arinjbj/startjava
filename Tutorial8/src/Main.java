37e6725f3ccaa05ad740391e84d3d51b8513c8c2
ArinJBJ <qwert1424@naver.com>
2020-07-28 17:41:31 +0900

    java 기초 복습

---
+++ Tutorial8/src/Main.java
@@ -0,0 +1,31 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Arinjbj");
+		
+		////////
+		
+		System.out.println("20");
+		System.out.println("900");
+		System.out.println("15");
+		System.out.println("13");
+		System.out.println("17");
+		
+		////////
+		
+		for(int i = 0; i < 10 ; i++)
+		{
+			for(int j = 0 ; j < 10 ; j++)
+			{
+				System.out.print("*");
+				System.out.print(" ");
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
